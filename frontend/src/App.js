bf0d5fb1d3c8502b34e180f9353f8cd15cdee121
Martin Valchev <mv1u18@soton.ac.uk>
2021-03-01 00:09:24 +0200

    f1 over iterations chart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import './App.css';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Label, ReferenceLine, ResponsiveContainer } from 'recharts';
 
 const io = require("socket.io-client");
 const socket = io('http://127.0.0.1:5000', {transports: ['websocket']});
@@ -26,16 +27,27 @@ function App() {
   const [progress, setProgress] = useState(0);
   const [total, setTotal] = useState(0);
   const [score, setScore] = useState(0.00);
+  const [targetScore, setTargetScore] = useState(80.00);
+  const [scoreSeries, setScoreSeries] = useState([]);
 
   useEffect(() => {
     socket.on("tweet", data => {
       setTweet(data)
     });
-  });
+  }, []);
+
+  useEffect(() => {
+    socket.on("score-data", data => {
+      setScoreSeries(score => [...score, data]);
+    });
+  }, []);
 
   return (
     <div className="App">
       <div className="App-main">
+        <p>
+          {progress} out of {total} data points labelled
+        </p>
         <div className="tweet">
           <span>{tweet.text}</span>
         </div>
@@ -48,11 +60,43 @@ function App() {
           <span>{uncertainty * 100}%</span>
         </div>
         <p>
-          {progress} out of {total} data points labelled.
-        </p>
-        <p>
           Current classification performance (f1-score): {score * 100}% 
         </p>
+        <ResponsiveContainer width="50%" height="50%">
+          <LineChart
+            width={500}
+            height={300}
+            data={scoreSeries}
+            margin={{
+              top: 10,
+              right: 45,
+              left: 45,
+              bottom: 45,
+            }}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="labels"
+              type="number" 
+              domain={['dataMin', 'dataMax']}
+              tickFormatter={tick => `${tick}%`}
+              padding={{ left: 20, right: 20 }}>
+              <Label value="labeled ratio" offset={-30} position="insideBottom" fill="#82ca9d" />
+            </XAxis>
+            <YAxis type="number"
+              domain={[0, 100]}
+              tickFormatter={tick => `${tick}%`}>
+              <Label value="f1-score" angle={-90} offset={-30} position="insideLeft" fill="#82ca9d" />
+            </YAxis>
+              <Tooltip formatter={score => [`${score}%`, "f1-score"]}
+              labelStyle={{color: "#282c34"}}
+              labelFormatter={label => `labeled: ${label}%,`}
+              contentStyle={{borderRadius: "9px", fontSize: "18px", backgroundColor: "rgba(248, 248, 248, 0.85)", lineHeight: "20px"}}
+              itemStyle={{color: "#282c34"}}/>
+            <ReferenceLine y={targetScore} stroke="#8884d8">
+              <Label value="Learning Target" fill="#8884d8" position="top" />
+            </ReferenceLine>
+            <Line type="monotone" dataKey="score" stroke="#82ca9d" activeDot={{ r: 8 }} />
+          </LineChart>
+        </ResponsiveContainer>
       </div>
     </div>
   );
