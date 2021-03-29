a75757e42c80474abb02793e5a1b2a6d626fef4c
Martin Valchev <mv1u18@soton.ac.uk>
2021-03-29 20:13:54 +0300

    automatic active learning

--- backend/active_learning.py
+++ backend/active_learning.py
@@ -1,15 +1,15 @@
 import logging
-import random
 import hashlib
 
 from typing import Callable, Collection, Dict, Tuple, Union
 
 from sklearn.base import BaseEstimator
 
-from backend.sio_server import Server
-from backend.learning import Learning, Vectorizer
+from sio_server import Server
+from learning import Learning, Vectorizer
 
 import pandas as pd
+pd.options.mode.chained_assignment = None  # default='warn'
 from pandas.core.frame import DataFrame
 import numpy as np
 
@@ -28,11 +28,13 @@ class ActiveLearning(Learning):
                  vectorizer: Vectorizer = None,
                  extra_processing: Callable[[DataFrame], DataFrame] = None,
                  start: bool = False,
-                 target_score: float = 80.00):
+                 target_score: float = 80.00,
+                 n_queries: int = None):
         super().__init__(estimator, dataset, columns, vectorizer, extra_processing)
         assert callable(query_strategy), 'query_strategy must be callable'
         self.query_strategy = query_strategy
         self.target_score = target_score
+        self.n_queries = n_queries
         # start webserver 
         if start: self.start()
 
@@ -42,64 +44,88 @@ class ActiveLearning(Learning):
         self.learn_text_model()
         self.partition()
         self.split(self.labeled_pool)
-        self.setup_pool()
         # initialise active learner model
         self.learner = ActiveLearner(
             estimator = self.estimator,
             query_strategy = self.query_strategy,
-            #X_training = self.X_train, y_training = self.y_train
+            X_training = self.X_train, y_training = self.y_train
         )
+        self.auto_teach(self.n_queries)
         self.start_server()
 
     def split(self, pool: DataFrame, y: str = 'target'):
         # split into training and testing subsets
         # ensures at least 5 samples per class for initial training and testing
-        if self.labeled_size >= 100:
+        labels = len(self.labeled_pool.index)
+        if labels >= 100:
             X_unlabled, X_train_test = train_test_split(pool, random_state=42, test_size=0.2, stratify=pool[y])
-            self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X_train_test.drop(y, axis=1), X_train_test[y], random_state=42, test_size=0.5, stratify=X_train_test[y])
+            self.X_train_raw, self.X_test_raw, self.y_train, self.y_test = train_test_split(X_train_test.drop(y, axis=1), X_train_test[y], random_state=42, test_size=0.5, stratify=X_train_test[y])
+            self.X_train, self.X_test = self.build_features(self.X_train_raw, self.columns), self.build_features(self.X_test_raw, self.columns)
+
+            self.y_train = self.y_train.to_numpy()
 
             # unlabel 80%
             X_unlabled[y] = np.nan
 
             # merge with previously unlabled instances if any
-            self.X_raw = pd.concat([self.unlabeled_pool, X_unlabled], ignore_index=True)
+            X_pool = pd.concat([X_unlabled, self.unlabeled_pool])
 
-            self.X_raw = self.X_raw.drop(y, axis=1)
-            self.y = self.X_raw[y]
-
-        elif self.labeled_size >= 20:
-            self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(pool.drop(y, axis=1), pool[y], random_state=42, test_size=0.2, stratify=pool[y])
+            self.X_pool_raw = X_pool.drop(y, axis=1)
+            self.X_pool = self.build_features(self.X_pool_raw, self.columns)
+            self.labeled_size = len(self.X_train_raw.index)
+            self.dataset_size = len(self.dataset.index) - len(self.X_test_raw.index)
+        elif labels >= 20:
+            self.X_train_raw, self.X_test_raw, self.y_train, self.y_test = train_test_split(pool.drop(y, axis=1), pool[y], random_state=42, test_size=0.2, stratify=pool[y])
+            self.X_train, self.X_test = self.build_features(self.X_train_raw, self.columns), self.build_features(self.X_test_raw, self.columns)
+            self.X_pool_raw = self.unlabeled_pool.drop(y, axis=1)
+            self.X_pool = self.build_features(self.X_pool_raw, self.columns)
+            self.labeled_size = len(self.X_train_raw.index)
+            self.dataset_size = len(self.dataset.index) - len(self.X_test_raw.index)
         else:
-            self.X_raw, self.y = self.unlabeled_pool.drop('target', axis=1), self.unlabeled_pool.target
+            self.X_pool_raw = self.unlabeled_pool.drop(y, axis=1)
+            self.X_pool = self.build_features(self.X_pool_raw, self.columns)
+            self.labeled_size = 0
+            self.dataset_size = len(self.dataset.index)
             #raise Exception("Not enough labeled samples to fit classifier and generate test set")
+        
+    def auto_teach(self, queries = None):
+        for _ in range(0, queries) if queries else range(0, self.X_pool.shape[0]):
+            # retrieve most uncertain instance
+            idx, sample = self.learner.query(self.X_pool)
+            idx = int(idx)
+            raw_idx = self.X_pool_raw.iloc[idx].name
+            self.learner.teach(sample, np.array([self.labeled_pool.loc[raw_idx].target], dtype=int))
+            self.labeled_size += 1
+            # remove learned sample from pool
+            self.X_pool_raw = self.X_pool_raw.drop(raw_idx)
+            self.X_pool = drop_rows(self.X_pool, idx)
+            # store accuracy metric after training
+            self.accuracy_scores.append({'queries': self.labeled_size, 'score': self.learner.score(self.X_test, self.y_test)*100})
 
     # utility function used to teach an active learner a new sample tweet from a json object {idx: i, hash: h, label: l}
     # after being learned, the tweet is removed from the sampling pool and the new model performance is recorded
-    def teach(self, tweet: Dict[str, Union[int, str]]):
+    def teach(self, tweet: Dict[str, Union[int, str]], hashed = False):
         # extract data from tweet obj
         idx = int(tweet['idx'])
         label = int(tweet['label'])
-        if self.X_raw.empty: return
-        text: str = self.X_raw.iloc[idx].tweet
+        if self.X_pool_raw.empty: return
+        text: str = self.X_pool_raw.iloc[idx].tweet
         y_new: np.ndarray = np.array([label], dtype=int)
 
         # fail early if hashes don't match (web app out of sync)
-        if tweet['hash'] != hashlib.md5(text.encode()).hexdigest(): return
+        if hashed and tweet['hash'] != hashlib.md5(text.encode()).hexdigest(): return
 
         # teach new sample
-        logging.info(f'-# Teaching instance: \n idx {idx}, \n label {label}, \n tweet: {text}, \n words: {self.X_raw.iloc[idx].tweet_clean} #-')
-        self.learner.teach(self.__get_row__(self.X, idx), y_new)
+        #logging.info(f'-# Teaching instance: \n idx {idx}, \n label {label}, \n tweet: {text}, \n words: {self.X_pool_raw.iloc[idx].tweet_clean} #-')
+        self.learner.teach(self.__get_row__(self.X_pool, idx), y_new)
         self.labeled_size += 1
 
         # remove learned sample from pool
-        self.X_raw = self.X_raw.drop(idx).reset_index(drop=True)
-        self.y = self.y.drop(idx).reset_index(drop=True)
-        self.X = drop_rows(self.X, idx)
+        self.X_pool_raw = self.X_pool_raw.drop(self.X_pool_raw.iloc[idx].name)
+        self.X_pool = drop_rows(self.X_pool, idx)
 
         # store accuracy metric after training
-        # TODO: obtain test sets
-        # accuracy_scores.append({'queries': self.labeled_size, 'score': learner.score(self.X_test, self.y_test)*100})
-        self.accuracy_scores.append({'queries': self.labeled_size, 'score': random.random()*100})
+        self.accuracy_scores.append({'queries': self.labeled_size, 'score': self.learner.score(self.X_test, self.y_test)*100})
 
     # starts an instance of the backend server used by the labeling web-app
     def start_server(self):
